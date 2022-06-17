efcea58da067911cfa5dddb84d58cac46cc12b56
Anton <98453976+anton12042001@users.noreply.github.com>
2022-06-17 12:11:39 +0700

    creating user authorization

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import "./Header.css";
+import classes from "./Header.module.css";
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
-        <header className='header'>
+        <header className={classes.header}>
             <img
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Vanamo_Logo.svg/1200px-Vanamo_Logo.svg.png"
                 alt=""/>
+            <div className={classes.loginBlock}>
+                {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>
+                }
+            </div>
         </header>
     );
 };
